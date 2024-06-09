5f5c44621792faf0618af2f6a5b0f5e4d2570de6
Steveaung3 <96459037+Steveaung3@users.noreply.github.com>
2024-06-09 21:15:08 +0630

    Update loader.js

--- assets/js/loader.js
+++ assets/js/loader.js
@@ -2,7 +2,7 @@ $(window).load(function() {
 	$('#loader').addClass('is_active');
 	setTimeout(function() {
 		$('#loader').addClass('is_loaded');
-	}, 4000);
+	}, 5000);
 	setTimeout(function() {
 		$('#loader_second').addClass('is_loaded');
 	}, 1800);
